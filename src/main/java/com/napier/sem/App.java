3a073099d0419a52773bc5e156b5d113cedb261f
Fabian-Skyner <40596430@live.napier.ac.uk>
2024-02-22 15:13:33 +0000

    BROKEN - changed pom.xml to implement JAR stuff

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,6 +5,7 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 
+@SuppressWarnings("ALL")
 public class App
 {
     public static void main(String[] args)
